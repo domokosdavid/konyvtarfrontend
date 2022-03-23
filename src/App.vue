bb81292402f51bea7588d1eba64c2df7eab3824a
Suli <suli>
2022-03-23 17:23:07 +0100

    vue kesz

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav>
+      <a href="#">Új könyv</a>
+      <a href="https://petrik.hu">Petrik honlap</a>
+    </nav>
+    <h1 style="text-align: left">Petrik Könyvtár Nyilvántartó</h1>
+    <div class="row">
+      <div
+        class="col-sm-12 col-md-6 col-lg-4"
+        v-for="book in books"
+        v-bind:key="book.id"
+        id="kartya"
+      >
+        <h3>{{ book.title }}</h3>
+        <p>{{ book.author }}</p>
+        <p>Kiadási év: {{ book.publish_year }}</p>
+        <p>Hossz: {{ book.page_count }} oldal</p>
+      </div>
+    </div>
+
+    <button @click="loadData">Betöltés</button>
+    <footer>
+      <p>Készítette: Domokos Dávid</p>
+    </footer>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+<style>
+#kartya {
+  border: 1px solid black;
+  text-align: left;
+  margin-bottom: 1%;
+  padding: 1%;
 }
+nav a {
+  padding-left: 30px;
+}
+footer p{
+  float: left;
+}
+</style>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      mod_new: true,
+      saving: false,
+      book: {
+        title: "",
+        author: "",
+        publish_year: "",
+        page_count: "",
+      },
+      books: [],
+    };
+  },
+  methods: {
+    async loadData() {
+      let Response = await fetch("http://127.0.0.1:8000/api/books");
+      let data = await Response.json();
+      this.books = data;
+    },
+  },
+};
 </script>
 
 <style>
